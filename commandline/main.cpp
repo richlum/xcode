5db6c789238015bb565f63841abdc3c1b35b281a
Richard Lum <richlum@richards-macbook.local>
2015-09-20 16:23:52 -0700

    Initial Commit

---
+++ commandline/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  commandline
+//
+//  Created by Richard Lum on 2015-09-20.
+//  Copyright © 2015 Richard Lum. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
